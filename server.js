10375152e61355f501826ced464202e112855241
Robert Cope <robert@copesystems.com>
2019-04-07 20:14:07 -0600

    Just a little bit of clean up, fixed the existing test, and got everything set up to additionall run in Travis CI.

--- server.js
+++ server.js
@@ -1,29 +1,40 @@
-const express = require('express')
-//const foobar_routes = require('routes/foobar.js'); //test api
-const path = require('path')
-const PORT = process.env.PORT || 5000
-const { Pool } = require('pg');
-const pool = new Pool({
-      connectionString: process.env.DATABASE_URL,
-      ssl: true
-});
+const express = require('express');
+const path = require('path');
+const pg = require('pg');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/db', async (req, res) => {
-      try {
-          const client = await pool.connect()
-          const result = await client.query('SELECT * FROM test_table');
-          const results = { 'results': (result) ? result.rows : null};
-          res.render('pages/db', results );
-          client.release();
-      }catch (err) {
-          console.error(err);
-          res.send("Error " + err);
-      }
-  })
-  //.get('/foobar',foobar_routes.foobar)  
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+const PORT = process.env.PORT || 5000;
+const DATABASE_URL = process.env.DATABASE_URL || "postgres://postgres@127.0.0.1/wifiology";
+
+
+
+function createApplication(pg_conn_str){
+    let application = express();
+    let pool = new pg.Pool({
+       connectionString: pg_conn_str,
+       ssl: true
+    });
+
+    application
+        .use(express.static(path.join(__dirname, 'public')))
+        .set('views', path.join(__dirname, 'views'))
+        .set('view engine', 'ejs')
+        .get('/', (req, res) => res.render('pages/index'))
+        .get('/db', async (req, res) => {
+            try {
+                const client = await pool.connect();
+                const result = await client.query('SELECT * FROM test_table');
+                const results = { 'results': (result) ? result.rows : null};
+                res.render('pages/db', results );
+                client.release();
+            } catch (err) {
+                console.error(err);
+                res.send("Error " + err);
+            }
+        });
+        //.get('/foobar',foobar_routes.foobar)
+
+    return application;
+}
+
+application = createApplication(DATABASE_URL).listen(PORT, () => console.log(`Listening on ${ PORT }`));
