52ca58b1d85549e2d8507e29681f1fc6c3f1564b
jjniemeyer <jani5714@colorado.edu>
2019-04-15 21:14:52 -0600

    initial sendgrid setup

--- server.js
+++ server.js
@@ -20,7 +20,15 @@ const securityAuthHandlerConstructor = require('./api/securityHandler');
 const PORT = process.env.PORT || 5000;
 const DATABASE_URL = process.env.DATABASE_URL || "postgres://postgres@127.0.0.1/wifiology";
 
+//sendgrid constants
+const helper = require('sendgrid').mail;
+const from_email = new helper.Email('jani5714@colorado.edu');
 
+//these are the test constants...need to change values
+const to_email = new helper.Email('roco9727@colodado.edu');
+const subject = 'Hello World from the SendGrid Node.js Library!';
+const content = new helper.Content('text/plain', 'Hello, Email!');
+const mail = new helper.Mail(from_email, subject, to_email, content);
 
 function createApplication(pg_conn_str){
     let application = express();
@@ -43,6 +51,19 @@ function createApplication(pg_conn_str){
                 res.send("Error " + err);
             }
         });
+
+    var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+    var request = sg.emptyRequest({
+        method: 'POST',
+        path: '/v3/mail/send',
+        body: mail.toJSON(),
+    });
+
+    sg.API(request, function(error, response) {
+        console.log(response.statusCode);
+        console.log(response.body);
+        console.log(response.headers);
+    });
 
     application.use(
         '/api/1.0/ui',
